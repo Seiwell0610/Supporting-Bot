4674d9abd089b092237a42c891854b33ea123d15
Seiwell <57401916+Seiwell0610@users.noreply.github.com>
2020-08-12 13:36:35 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import json
+import discord
+import asyncio
+import traceback
+from discord.ext import commands
+
+with open('setting.json', mode='r', encoding='utf-8') as fh:
+    json_txt = fh.read()
+    json_txt = str(json_txt).replace("'", '"').replace('True', 'true').replace('False', 'false')
+    token = json.loads(json_txt)['token']
+    prefix = json.loads(json_txt)['prefix']
+loop = asyncio.new_event_loop()
+
+async def run():
+    bot = MyBot()
+    try:
+        await bot.start(token)
+    except KeyboardInterrupt:
+        await bot.logout()
+
+
+class MyBot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=commands.when_mentioned_or(prefix), loop=loop)
+        self.remove_command('help')
+
+    async def on_ready(self):
+        for extension in ["guild_logs", "help", "admin"]:
+            try:
+                self.load_extension(f"cogs.{extension}")
+            except commands.ExtensionAlreadyLoaded:
+                self.reload_extension(f"cogs.{extension}")
+
+
+    async def on_command_error(self, ctx, error1):
+        if isinstance(error1, (commands.CommandNotFound, commands.CommandInvokeError)):
+            return
+
+if __name__ == '__main__':
+    try:
+        print("Logged in as")
+
+        main_task = loop.create_task(run())
+        loop.run_until_complete(main_task)
+        loop.close()
+
+    except Exception as error:
+        print("エラー情報\n" + traceback.format_exc())
